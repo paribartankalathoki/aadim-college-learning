5895411e4f7a2e42886bd375522a0b8e50dbb6e4
Paribartan Kalathoki <paribartankalathoki@gmail.com>
2022-12-02 08:31:19 +0545

    added base setup and forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DatePipe} from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TutorialsComponent } from './tutorials/tutorials.component';
+import { AddTutorialsComponent } from './add-tutorials/add-tutorials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderNavComponent,
+    DashboardComponent,
+    TutorialsComponent,
+    AddTutorialsComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
