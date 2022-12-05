bfbc06f8833e0b1d6bae9fc22a7e1311e77bba20
Paribartan Kalathoki <paribartankalathoki@gmail.com>
2022-12-05 07:40:32 +0545

    added user service and method to save new users

--- src/app/user-management/add-user/add-user.component.ts
+++ src/app/user-management/add-user/add-user.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Location} from '@angular/common';
+import {UsersService} from '../services/users.service';
+import {AddUserRequestModel} from '../models/add-user-request.model';
 
 @Component({
   selector: 'app-add-user',
@@ -15,7 +17,8 @@ export class AddUserComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private location: Location
+    private location: Location,
+    private userService: UsersService
   ) { }
 
   ngOnInit(): void {
@@ -43,5 +46,6 @@ export class AddUserComponent implements OnInit {
   onSaveDetails() {
     this.submitted = true;
     console.log("user details: ", this.formDetails.value);
+    this.userService.saveUser(this.formDetails.value as AddUserRequestModel);
   }
 }
